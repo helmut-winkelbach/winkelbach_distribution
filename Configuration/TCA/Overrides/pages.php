13925314c35e46530e5053573dfbc39b02b5eff5
helmut-winkelbach <helmut-winkelbach@users.noreply.github.com>
2023-01-07 18:10:12 +0100

    Update pages.php

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,20 +1,17 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'winkelbach_distribution';
+call_user_func(function() {
+ 
+     $extensionKey = 'winkelbach_distribution';
 
     /**
      * Default PageTS for WinkelbachDistribution
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        'winkelbach-distribution'
+        $extensionKey,
         'Configuration/TsConfig/Page/All.tsconfig',
-        'All-tsconfig'
+        'Winkelbach Distribution'
     );
 /**
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
