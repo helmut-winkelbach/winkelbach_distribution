926b327f81493ea477a1ecbe78e7a5710078c566
helmut-winkelbach <helmut-winkelbach@users.noreply.github.com>
2023-01-07 11:55:26 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
     'rte_ckeditor' => '10.4.0-11.5.99',
     'fluid_styled_content' => '10.4.0-11.5.99',
    ],
-   'conflicts' => [
+   'conflicts' => [ ],
   ],
   'clearcacheonload' => true,
 );
