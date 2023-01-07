90b2117533ce49fad8ffcd6b477a22fca2b465b2
helmut-winkelbach <helmut-winkelbach@users.noreply.github.com>
2023-01-07 18:12:12 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,11 +1,8 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
+call_user_func(function() {
+
     $extensionKey = 'winkelbach_distribution';
 
     /**
@@ -14,6 +11,6 @@ call_user_func(function()
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
-        'winkelbach-distribution'
+        'Winkelbach Distribution'
     );
 });
